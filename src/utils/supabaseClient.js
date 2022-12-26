6c564315d38906bb63589c6ef4e7f1e29c212216
Hachikoi-the-creator <facka72@gmail.com>
2022-12-26 14:45:01 -0600

    trying to fix env

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zeigbouzopuhpdcinrli.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_API_KEY;
+const supabaseAnonKey =
+  import.meta.env.VITE_API_KEY || process.env.VITE_API_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
